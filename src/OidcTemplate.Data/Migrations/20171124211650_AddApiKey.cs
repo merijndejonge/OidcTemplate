4fb0a304c1bd1c43fddff27c166711409a8e8078
Merijn de Jonge <merijn.de.jonge@gmail.com>
2017-11-25 00:07:27 +0100

    add ApplicationId

---
+++ src/OidcTemplate.Data/Migrations/20171124211650_AddApiKey.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace OpenSoftware.OidcTemplate.Data.Migrations
+{
+    public partial class AddApiKey : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ApiKey",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ApiKey",
+                table: "AspNetUsers");
+        }
+    }
+}
--- src/OidcTemplate.Domain/Entities/ApplicationUser.cs
+++ src/OidcTemplate.Domain/Entities/ApplicationUser.cs
@@ -6,5 +6,7 @@ namespace OpenSoftware.OidcTemplate.Domain.Entities
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
+
+        public string ApiKey { get; set; }
     }
 }
