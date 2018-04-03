8aded4d8b43df304cc1e3916876e585fff93353c
Merijn de Jonge <merijn.de.jonge@gmail.com>
2018-04-03 21:11:05 +0200

    Initial template generated with dotnet new razor --auth individual

--- src/OidcTemplate.Auth/Program.cs
+++ src/OidcTemplate.Auth/Program.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-using OpenSoftware.OidcTemplate.Auth.DatabaseSeed;
-using Serilog;
-using Serilog.Events;
 
 namespace OpenSoftware.OidcTemplate.Auth
 {
@@ -12,35 +14,12 @@ namespace OpenSoftware.OidcTemplate.Auth
     {
         public static void Main(string[] args)
         {
-            Console.Title = "OidcTemplate.Auth";
-
-            Log.Logger = new LoggerConfiguration()
-                .MinimumLevel.Debug()
-                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
-                .MinimumLevel.Override("System", LogEventLevel.Warning)
-                .MinimumLevel.Override("Microsoft.AspNetCore.Authentication", LogEventLevel.Verbose)
-                .MinimumLevel.Override("IdentityServer4", LogEventLevel.Debug)
-                .Enrich.FromLogContext()
-                .WriteTo.Console(
-                    outputTemplate:
-                    "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}"
-                )
-                .CreateLogger();
-
-            var host = BuildWebHost(args);
-            host.SeedDatabase();
-            host.Run();
+            BuildWebHost(args).Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
-                .UseUrls("http://localhost:5000")
-                .ConfigureLogging(builder =>
-                {
-                    builder.ClearProviders();
-                    builder.AddSerilog();
-                })
                 .Build();
     }
 }
--- src/OidcTemplate.Auth/Services/EmailSender.cs
+++ src/OidcTemplate.Auth/Services/EmailSender.cs
@@ -1,4 +1,7 @@
-﻿using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace OpenSoftware.OidcTemplate.Auth.Services
 {
